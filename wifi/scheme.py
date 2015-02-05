35775c0924ecc122401595c169661e6b24d40b0d
Gina Häußge <osd@foosel.net>
2015-02-05 17:49:23 +0100

    WEP passkeys in plaintext need to be prefixed with "s:" in order to function
    Since we also handle WPA keyphrases as plain text keyphrases, it makes sense to equally preprocess the entered data from the user here too, instead of forcing him to manually add s: in front of plain text keyphrases.
    
    Alternatively the documentation should hint the user that this is necessary.

--- wifi/scheme.py
+++ wifi/scheme.py
@@ -31,7 +31,7 @@ def configuration(cell, passkey=None):
         elif cell.encryption_type == 'wep':
             return {
                 'wireless-essid': cell.ssid,
-                'wireless-key': passkey,
+                'wireless-key': 's:' + passkey,
             }
         else:
             raise NotImplementedError
